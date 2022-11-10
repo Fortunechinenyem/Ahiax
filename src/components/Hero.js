06767835aed82f910aa4f05c78edd28d321f0acd
Fortunechinenyem <fortunechinenyem@gmail.com>
2022-11-10 08:33:35 +0100

    update

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,12 +1,14 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 const Hero = () => {
   return (
     <section className="hero ">
       <div className="">
         <div className="herotext">
           <h1>One Platform, Limitless Potential! </h1>
-          <button className="herobutton">Explore</button>
+          <Link to="/platform" className="btn btn-light mt-4">
+            Explore
+          </Link>
         </div>
       </div>
     </section>
