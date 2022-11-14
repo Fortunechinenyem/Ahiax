13afa071f6e7a250e6cb1e193a7c5a77a0362f22
Fortunechinenyem <fortunechinenyem@gmail.com>
2022-11-14 16:20:24 +0100

    update

--- src/components/PartnerHero.js
+++ src/components/PartnerHero.js
@@ -1,16 +1,27 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import logo from "../image/partnera.png";
 
 const PartnerHero = () => {
   return (
-    <section className="partnerhero ">
-      <div className="">
-        <div className="herotext">
-          <h1>Earn More Per Click With Ahiax</h1>
+    <section className="container ">
+      <div className="d-flex row g-5 mx-auto ">
+        <img
+          src={logo}
+          alt={PartnerHero.png}
+          className="col-md "
+          height=""
+          style={{ objectFit: "cover" }}
+        />
+        <div className="col-md text-center">
+          <h1>Earn More Per Click With Ahiax​</h1>
           <p>
             Take advantage of our various performance marketing plans to drive
             more quality leads and sales.
           </p>
-          <button className="btn btn-light">See Packages</button>
+          <Link to="/platform" className="btn btn-success mt-4 ">
+            See Packages
+          </Link>
         </div>
       </div>
     </section>
