a44c41c0b8cb28d83158d8b52cfcc59b82f4148b
Fortunechinenyem <fortunechinenyem@gmail.com>
2022-11-14 16:14:37 +0100

    update

--- src/components/BrandHero.js
+++ src/components/BrandHero.js
@@ -1,17 +1,25 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import logo from "../image/ba.png";
 
 const BrandHero = () => {
   return (
-    <section className="brandhero ">
-      <div className="">
-        <div className="herotext">
-          <h1>​Acquire customers and build your business in one place </h1>
+    <section className="container ">
+      <div className="d-flex row g-5 mx-auto ">
+        <img
+          src={logo}
+          alt={BrandHero.png}
+          className="col-md "
+          height=""
+          style={{ objectFit: "cover" }}
+        />
+        <div className="col-md text-center">
+          <h1>​Acquire customers and build your business in one place </h1>
           <p>We've set our standards high for you and keep them there.</p>
+          <Link to="/platform" className="btn btn-success mt-4 ">
+            Grow your Sales
+          </Link>
         </div>
-        <Link to="/platform" className="btn btn-light mt-4">
-          Grow your Sales
-        </Link>
       </div>
     </section>
   );
