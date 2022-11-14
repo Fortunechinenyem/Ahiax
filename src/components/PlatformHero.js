027f892842279d7d43aaa504c4fda77be6690473
Fortunechinenyem <fortunechinenyem@gmail.com>
2022-11-14 16:06:40 +0100

    update

--- src/components/PlatformHero.js
+++ src/components/PlatformHero.js
@@ -1,12 +1,21 @@
 import React from "react";
+import logo from "../image/platformhero.PNG";
 
 const PlatformHero = () => {
   return (
-    <section className="platformhero ">
-      <div className="">
-        <div className="herotext">
-          <h1>Ours Approach is to Push Industries and Partners Forward </h1>
-        </div>
+    <section className=" text-center container">
+      <div className="d-flex row g-5 mx-auto ">
+        <img
+          src={logo}
+          alt={PlatformHero.PNG}
+          className="col-md "
+          height=""
+          style={{ objectFit: "cover" }}
+        />
+
+        <h1 className="herotext col-md">
+          Our Approach is to Push Industries and Partners Forward
+        </h1>
       </div>
     </section>
   );
