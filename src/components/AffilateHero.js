2403a36d3696baceb59635095561214d123a192d
Fortunechinenyem <fortunechinenyem@gmail.com>
2022-11-16 14:58:26 +0100

    update

--- src/components/AffilateHero.js
+++ src/components/AffilateHero.js
@@ -1,28 +1,23 @@
 import React from "react";
-
-import logo from "../image/afa.PNG";
+import { Link } from "react-router-dom";
 
 const AffiliateHero = () => {
   return (
-    <section className="container ">
-      <div className="d-flex row g-5 mx-auto ">
-        <img
-          src={logo}
-          alt={AffiliateHero.png}
-          className="col-md "
-          height=""
-          style={{ objectFit: "cover" }}
-        />
-        <div className="col-md text-center">
-          <h1>​The Power  of 'Affiliate'</h1>
+    <section className="affiliatehero ">
+      <div className="">
+        <div className="herotext">
+          <h1>The Power of 'Affiliate' </h1>
           <p>
-            ​Affiliate marketing is a performance-driven channel that enables a
+            Affiliate marketing is a performance-driven channel that enables a
             rich ecosystem of partnerships to reach, influence, and convert
             shoppers worldwide at every part of their purchase journey, both
             online and offline. ElanX is the place where partnerships and
             technology converge to create valuable shopping experiences that
             result in scalable, profitable growth.
           </p>
+          <Link to="/affiliateform" className="btn btn-light mt-4">
+            Become An Affiliate
+          </Link>
         </div>
       </div>
     </section>
